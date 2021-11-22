676b1e4cf53ad2c2732c8a5da19289cc7c53c42b
chaizafran97 <82008745+chaizafran97@users.noreply.github.com>
2021-11-22 23:22:24 +0800

    22/11 Changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from website import create_app
 
 app = create_app()
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 if __name__ == '__main__':
     app.run(debug=True)
--- website/models.py
+++ website/models.py
@@ -12,7 +12,14 @@ class Note(db.Model):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
+    matric = db.Column(db.String(10), unique=True)
     password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
+    name = db.Column(db.String(150))
+    tutor = db.Column(db.String(3))
     notes = db.relationship('Note')
+
+class Lessons(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(150))
+    participants = db.relationship('User')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
