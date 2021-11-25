8b6aadc6e14c9e8915540f321199f52f6a2f570b
chaizafran97 <82008745+chaizafran97@users.noreply.github.com>
2021-11-25 20:57:10 +0800

    database updates

--- website/models.py
+++ website/models.py
@@ -7,18 +7,26 @@ class Student(db.Model, UserMixin):
     matric = db.Column(db.String(10), unique=True)
     password = db.Column(db.String(150))
     name = db.Column(db.String(150))
+    kulliyah = db.Column(db.String(5))
+    program = db.Column(db.String(5))
+    email = db.Column(db.String(100))
     student_lesson = db.Column(db.Integer, db.ForeignKey('lessons.lesson_id'))
 
 
 class Lessons(db.Model):
     lesson_id = db.Column(db.Integer, primary_key=True)
-    subject = db.Column(db.String(150))
+    subject_name = db.Column(db.String(150))
+    subject_code = db.Column(db.String(15))
     participants = db.relationship('Student')
+    start_time = db.Column(db.DateTime)
+    end_time = db.Column(db.DateTime)
 
 
 class Tutors(db.Model):
     tutor_id = db.Column(db.Integer, primary_key=True)
-    lessons = db.Column(db.String(100))
+    matric = db.Column(db.Integer)
     password = db.Column(db.String(150))
     name = db.Column(db.String(100))
-    matric = db.Column(db.Integer)
+    kulliyah = db.Column(db.String(5))
+    email = db.Column(db.String(100))
+    lessons = db.Column(db.String(100))
